0824ac2569577ad6652b9471f9608133fb48fc6c
Sakshi Bhandarkar <sakshi300699.b@gmail.com>
2021-01-28 14:12:58 +0530

    Add config file

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $username="root";
 $password="";
 $con= mysqli_connect($server,$username,$password,'feminine');
 if(!$con){
-    die("Connection not made due to".mqsqli_connect_error());
+    die("Error : Connection not made due to".mqsqli_connect_error());
 }
 
 ?>
