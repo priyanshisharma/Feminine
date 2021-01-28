d161dd528a07c7518fc5ca981aaa7991fbba0f71
AnjaliPatle <anjalipatle1417@gmail.com>
2021-01-28 20:54:30 +0530

    Add consult styling

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
     
 $server= "localhost";
 $username="root";
-$password="";
+$password="1234567";
 $con= mysqli_connect($server,$username,$password,'feminine');
 if(!$con){
     die("Error : Connection not made due to".mqsqli_connect_error());
