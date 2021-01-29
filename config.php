f003156b8e38361df769222334809620b0280812
Sakshi Bhandarkar <sakshi300699.b@gmail.com>
2021-01-30 03:52:42 +0530

    final update

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
     
 $server= "localhost";
 $username="root";
-$password="1234567";
+$password="";
 $con= mysqli_connect($server,$username,$password,'feminine');
 if(!$con){
     die("Error : Connection not made due to".mqsqli_connect_error());
